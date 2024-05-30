68bdef4670a5a430e829bb47c52da033d39463cf
HLoghnan <143848580+GamingN0body@users.noreply.github.com>
2024-05-30 10:29:31 -0700

    completed the small things
    I worked on mobile for each html and worked on fixing the little issues. I now have no bugs in the console.

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 // alert("Click on images to move through the carousels.")
 // If I can lets create a way to change the images html using JS onclick due to a button on top.
+function navIcon(x) {
+    x.classList.toggle("change");
+}
