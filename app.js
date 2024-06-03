d9188595604b8777b2cd18320fcb323fdbc7cc7a
HLoghnan <143848580+GamingN0body@users.noreply.github.com>
2024-06-02 18:26:34 -0700

    finished all the big things
    completed the gallery so that the images all fit and look good using grids., then went on to complete the colors that may or not be kept or deleted, and finally came about creating the contact form that is a clickable popup. All that is left for now is the little changes and bug fixes along with making sure mobile is working correct.

--- app.js
+++ app.js
@@ -1,5 +1,56 @@
-// alert("Click on images to move through the carousels.")
-// If I can lets create a way to change the images html using JS onclick due to a button on top.
-function navIcon(x) {
-    x.classList.toggle("change");
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
 }
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// filtering images (does not need to be changed)
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("gallery-column");
+    if (c == "all") c = "";
+    //removes the images that are not active and shows the images that are active
+    for (i = 0; i < x.length; i++) {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    }
+}
+
+//adds the classes that makes the images appear
+function AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+}
+
+// removes the classes that are not active
+function RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) { //while the active class is active all other classes will be not shown
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// lets the buttons to actually allow the code to function on the website
+var btnContainer = document.getElementById("btn-container");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+// changes the icon for the mobile
